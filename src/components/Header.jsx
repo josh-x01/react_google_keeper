2d0c33945c0d2368f1038e0b5930c3bedb9f14bb
Joshua <87242555+eyasuyid@users.noreply.github.com>
2022-10-05 23:29:41 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Highlight from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Highlight />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
